38bf954c494f3f29cc2a25241410d7cb075c304a
Eric <ejwalters24@gmail.com>
2021-05-06 20:31:19 -0400

    added collection view cells and profile image/name

--- InstagramFirestoreTutorial/Controller/FeedController.swift
+++ InstagramFirestoreTutorial/Controller/FeedController.swift
@@ -7,14 +7,50 @@
 
 import UIKit
 
+private let reuseIdentifier = "Cell"
 
-class FeedController: UIViewController {
+class FeedController: UICollectionViewController {
+    
+   
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemBlue
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(FeedCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
     
     
+}
+
+
+// MARK: - UICollectionViewDataSource
+
+extension FeedController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FeedCell
+        return cell
+    }
+    
+    
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
 }
