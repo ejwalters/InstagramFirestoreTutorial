0b1b2401831e0a20fc2b4792fafdc6e26ec344b0
Eric <ejwalters24@gmail.com>
2021-05-07 21:10:38 -0400

    added login ui

---
+++ InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by eriwalte on 5/7/21.
+//
+
+import UIKit
+
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+    
+}
