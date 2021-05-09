d0914ce7263688412f7bbc293450d76b63c004de
Eric <ejwalters24@gmail.com>
2021-05-08 21:26:08 -0400

    built search cells

--- InstagramFirestoreTutorial/Controller/SearchController.swift
+++ InstagramFirestoreTutorial/Controller/SearchController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class SearchController: UIViewController {
+private let reuseIdentifier = "UserCell"
+
+class SearchController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
         
-        view.backgroundColor = .systemPink
+    }
+    
+    // MARK: - Helpers
+    func configureTableView() {
+        view.backgroundColor = .white
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
     }
     
     
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 }
