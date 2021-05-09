d894b06345595fd1193b8d3b7d7e3bbfbc4f7ed6
Eric <ejwalters24@gmail.com>
2021-05-09 07:45:30 -0400

    added search for users to search controller

---
+++ InstagramFirestoreTutorial/ViewModel/UserCellViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  UserCellViewModel.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by eriwalte on 5/9/21.
+//
+
+import Foundation
+
+struct UserCellViewModel {
+    
+    private let user: User
+    var profileImageUrl: URL? {
+        return URL(string: user.profileImageUrl)
+    }
+    
+    var username: String {
+        return user.username
+    }
+    
+    var fullname: String {
+        return user.fullname
+    }
+    
+    init(user: User) {
+        self.user = user
+    }
+}
