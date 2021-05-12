230f91e7e7adf1ec22138b24fbc7e10e93c306f1
Eric <ejwalters24@gmail.com>
2021-05-11 20:08:47 -0400

    added logic to get keyboard and post buttons for comment ui

--- InstagramFirestoreTutorial/Controller/CommentController.swift
+++ InstagramFirestoreTutorial/Controller/CommentController.swift
@@ -15,11 +15,39 @@ class CommentController: UICollectionViewController {
     
     // MARK: - Properties
     
+    private lazy var commentInputView: CommentInputAccessoryView = {
+        
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
+        let cv = CommentInputAccessoryView(frame: frame)
+        
+        return cv
+        
+        
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
+    }
+    
+    override var inputAccessoryView: UIView?{
+        get { return commentInputView }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
     }
     
     // MARK: - Helpers
@@ -41,7 +69,6 @@ extension CommentController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CommentCell
-        cell.backgroundColor = .red
         return cell
     }
 }
