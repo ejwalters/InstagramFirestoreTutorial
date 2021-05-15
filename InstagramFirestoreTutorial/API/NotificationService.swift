4c19ca1df759d8e039ac46842b87c02dd11f9070
Eric <ejwalters24@gmail.com>
2021-05-15 08:25:32 -0400

    added logic to upload notification to firebase

---
+++ InstagramFirestoreTutorial/API/NotificationService.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationService.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by eriwalte on 5/15/21.
+//
+
+import Firebase
+
+struct NotificationService {
+    
+    static func uploadNotification(toUid uid: String, type: NotificationType, post: Post? = nil) {
+        
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        guard uid != currentUid else { return }
+        
+        var data: [String:Any] = ["timestamp": Timestamp(date: Date()),
+                                  "uid": currentUid,
+                                  "type": type.rawValue]
+        
+        if let post = post {
+            data["postId"] = post.postId
+            data["postImageUrl"] = post.imageUrl
+        }
+        
+        COLLECTION_NOTIFICATIONS.document(uid).collection("user-notifications").addDocument(data: data)
+    }
+    
+    static func fetchNotifications() {
+        
+    }
+}
