84501c4345594a69c2366a6b6ca8756510b23854
Eric <ejwalters24@gmail.com>
2021-05-15 08:43:13 -0400

    fetch notifications from firebase

--- InstagramFirestoreTutorial/Controller/NotificationsController.swift
+++ InstagramFirestoreTutorial/Controller/NotificationsController.swift
@@ -12,12 +12,25 @@ private let reuseIdentifier = "NotificationCell"
 class NotificationsController: UITableViewController {
     
     // MARK: - Properties
+    private var notifications = [Notification]() {
+        didSet{ tableView.reloadData() }
+    }
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        fetchNotifications()
+    }
+    
+    // MARK: - API
+    
+    func fetchNotifications() {
+        NotificationService.fetchNotifications { notifications in
+            self.notifications = notifications
+            print("DEBUG: Notifications \(notifications)")
+        }
     }
     
     
@@ -39,7 +52,7 @@ class NotificationsController: UITableViewController {
 extension NotificationsController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notifications.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
