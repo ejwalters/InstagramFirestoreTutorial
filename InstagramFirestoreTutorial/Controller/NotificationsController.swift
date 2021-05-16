06d2d1b325556b817210d86bf95723af40f512ca
Eric <ejwalters24@gmail.com>
2021-05-16 08:43:20 -0400

    adding touches to notifications table

--- InstagramFirestoreTutorial/Controller/NotificationsController.swift
+++ InstagramFirestoreTutorial/Controller/NotificationsController.swift
@@ -16,6 +16,8 @@ class NotificationsController: UITableViewController {
         didSet{ tableView.reloadData() }
     }
     
+    private let refresher = UIRefreshControl()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -46,6 +48,15 @@ class NotificationsController: UITableViewController {
     }
     
     
+    // MARK: - Actions
+    
+    @objc func handleRefresh() {
+        notifications.removeAll()
+        fetchNotifications()
+        refresher.endRefreshing()
+    }
+    
+    
     // MARK: - Helpers
     
     func configureTableView() {
@@ -55,6 +66,9 @@ class NotificationsController: UITableViewController {
         tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 80
         tableView.separatorStyle = .none
+        
+        refresher.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        tableView.refreshControl = refresher
     }
     
     
@@ -81,7 +95,9 @@ extension NotificationsController {
 
 extension NotificationsController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showLoader(true)
         UserService.fetchUser(withUid: notifications[indexPath.row].uid) { user in
+            self.showLoader(false)
             let controller = ProfileController(user: user)
             self.navigationController?.pushViewController(controller, animated: true)
         }
@@ -90,19 +106,25 @@ extension NotificationsController {
 
 extension NotificationsController: NoitificationCellDelegate {
     func cell(_ cell: NotificationCell, wantsToFollow uid: String) {
+        showLoader(true)
         UserService.follow(uid: uid) { _ in
+            self.showLoader(false)
             cell.viewModel?.notification.userIsFollowed.toggle()
         }
     }
     
     func cell(_ cell: NotificationCell, wantsToUnfollow uid: String) {
+        showLoader(true)
         UserService.unfollow(uid: uid) { _ in
+            self.showLoader(false)
             cell.viewModel?.notification.userIsFollowed.toggle()
         }
     }
     
     func cell(_ cell: NotificationCell, wantsToViewPost postId: String) {
+        showLoader(true)
         PostService.fetchPost(withPostId: postId) { post in
+            self.showLoader(false)
             let controller = FeedController(collectionViewLayout: UICollectionViewFlowLayout())
             controller.post = post
             self.navigationController?.pushViewController(controller, animated: true)
